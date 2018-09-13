36371cdb724f59ac6ba6ba327db106aa30999c5a
Mustafa TEKERASLAN <mustafa.tekeraslan@koda.com.tr>
2018-09-13 15:45:13 +0300

    Project uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FilterComponent } from './filter/filter.component';
+import { BarComponent } from './bar/bar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FilterComponent,
+    BarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
