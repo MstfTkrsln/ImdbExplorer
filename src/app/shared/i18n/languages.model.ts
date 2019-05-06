1827d05f9e24236c380905a6c9c64de247f18f65
Mustafa TEKERASLAN <mustafa.tekeraslan@gmail.com>
2019-05-06 11:50:39 +0300

    fixed order

--- src/app/shared/i18n/languages.model.ts
+++ src/app/shared/i18n/languages.model.ts
@@ -1,16 +1,16 @@
 export const languages: any = [
   {
-    'key': 'tr',
-    'key2': 'tr-TR',
-    'flag': 'tr',
-    'alt': 'Turkish',
-    'title': 'Türkçe'
-  },
-  {
     'key': 'en',
     'key2': 'en-US',
     'flag': 'us',
     'alt': 'United States',
     'title': 'English (US)'
+  },
+  {
+    'key': 'tr',
+    'key2': 'tr-TR',
+    'flag': 'tr',
+    'alt': 'Turkish',
+    'title': 'Türkçe'
   }
 ];
