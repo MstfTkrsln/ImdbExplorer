971ca218d5570d13d6251e31831fb2e3f4fb384b
Mustafa TEKERASLAN <mustafa.tekeraslan@gmail.com>
2019-05-07 16:57:32 +0300

    footer dev

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -10,8 +10,19 @@ export class FooterComponent implements OnInit {
   constructor() {
   }
 
-  ngOnInit() {
- 
+  ngOnInit() { 
   }
+
+  sendMail(){
+    window.location.href = 'mailto:info@imdbexplorer.com';
+  }
+
+  navigateTwitter(){
+    window.open('https://twitter.com/imdb_explorer');
+  }
+
+  navigateInstagram(){
+    window.open('https://instagram.com/imdb.explorer');
+  }  
 
 }
